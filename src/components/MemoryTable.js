08cf1bf26768f8ed433bd50d932132c74d35e4bf
ealbiol <eduard.albiol@gmail.com>
2022-08-10 16:44:37 +0200

    Adding sass and testing onClick function

--- src/components/MemoryTable.js
+++ src/components/MemoryTable.js
@@ -1,9 +1,11 @@
 import * as React from "react"
+import "../styles/MemoryTable.scss"
 import { getAllPieces } from "../data/data"
 
 export const MemoryTable = () => {
 
     const [pieces, setPieces] = React.useState([])
+    const [buttonStatus, setButtonStatus] = React.useState(false)
 
     React.useEffect(() => {
         getAllPieces().then((result) => {
@@ -11,16 +13,21 @@ export const MemoryTable = () => {
         })
     }, [])
 
+    const handleButtonPiece = () => {
+        setButtonStatus(!buttonStatus)
+        console.log("AA", buttonStatus);
+    }
 
     return (
-        <div>
+        <div className="table">
             {
                 pieces.map((piece, index) => {
-                    return (<div key={index}>
-                        <div>{piece.name !== "Earth" ? piece.name : ""}</div>
-
-                        {piece.name !== "Earth" && <div>{piece.name}</div>}
-                    </div>)
+                    return (
+                        <div key={index}>
+                            {/* <div style={{ backgroundColor: "salmon" }}>{piece.name !== "Earth" ? piece.name : ""}</div>
+                        {piece.name !== "Earth" && <div>{piece.name}</div>} */}
+                            <div className="pieceBox" onClick={() => handleButtonPiece()}>{piece.name}</div>
+                        </div>)
                 })
             }
         </div>
